754365f61b8b23d944fecafb2674d4cde790096a
amirgroot <amir1378khalili@gmail.com>
2023-12-28 19:03:06 +0330

    FIX route of app

--- client-note/note/src/App.tsx
+++ client-note/note/src/App.tsx
@@ -1,7 +1,8 @@
 // App.js
 
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Update the import statement
+import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
+import * as React from 'react';
 import { CSSReset } from '@chakra-ui/react';
 import Header from './components/Header';
 import NoteList from './components/Notes/NoteList';
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/notes/newNote" element={<AddNoteForm />} />
           <Route path="/notes" element={<NoteList />} />
+          <Route path="/" element={<Outlet />} />
         </Routes>
       </>
     </Router>
