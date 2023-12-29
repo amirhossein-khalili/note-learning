a536696f75d6ad28150ea4d38720d36e7e409e8b
amirgroot <amir1378khalili@gmail.com>
2023-12-29 23:43:20 +0330

    change navbar

--- client-note/note/src/components/NavBar.tsx
+++ client-note/note/src/components/NavBar.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Box, Flex, Spacer, Heading, Image } from '@chakra-ui/react';
+// import { Box, Flex, Spacer, Heading, Image } from '@chakra-ui/react';
+import { HStack, Image, Heading } from '@chakra-ui/react';
 import { EmailIcon } from '@chakra-ui/icons';
+import ColorModeSwitch from './ColorModeSwitch';
+import logo from '../assets/logo_transparent.webp';
 
 const NavBar: React.FC = () => {
   return (
-    <Box p="4" bg="green.300" color="white">
-      <Flex alignItems="center">
-        <Link to="/">
-          <Image src="/logo/logo_transparent.svg" alt="Logo" boxSize="100px" mr="2" />
-        </Link>
-        <Heading size="md">this is test for header </Heading>
-        <Spacer />
-        <Link to="/notes">
-          <EmailIcon aria-label="Notes" color="whiteAlpha" ml={2} boxSize={7} />
-        </Link>
-      </Flex>
-    </Box>
+    <HStack justifyContent="space-between" padding="10px" p="4" bg="green.300" color="white">
+      <Link to="/">
+        <Image src={logo} alt="Logo" boxSize="100px" mr="2" />
+      </Link>
+      <Heading size="md">this is test for header </Heading>
+      <Link to="/notes">
+        <EmailIcon aria-label="Notes" color="whiteAlpha" ml={2} boxSize={7} />
+      </Link>
+      <ColorModeSwitch />
+    </HStack>
   );
 };
 
