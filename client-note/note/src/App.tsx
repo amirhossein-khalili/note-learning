dd3e2493ff58d37067d8712ba56666b1db7090c3
amirgroot <amir1378khalili@gmail.com>
2023-12-29 14:42:45 +0330

    change app grid

--- client-note/note/src/App.tsx
+++ client-note/note/src/App.tsx
@@ -3,8 +3,8 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
 import * as React from 'react';
-import { CSSReset } from '@chakra-ui/react';
-import Header from './components/Header';
+import { CSSReset, Grid, GridItem, Show } from '@chakra-ui/react';
+import NavBar from './components/NavBar';
 import NoteGrid from './components/Notes/NoteGrid';
 import AddNoteForm from './components/Notes/AddNoteForm';
 // import NoteRoute from './components/Notes/NoteRoute';
@@ -13,8 +13,22 @@ function App() {
   return (
     <Router>
       <>
+        <Grid
+          templateAreas={{
+            base: `"nav" " main"`,
+            lg: `"nav nav" "aside main"`,
+          }}
+        >
+          <GridItem area="nav">
+            <NavBar />
+          </GridItem>
+          <Show above="lg">
+            <GridItem area="aside"></GridItem>
+          </Show>
+          <GridItem area="main"></GridItem>
+        </Grid>
         <CSSReset />
-        <Header />
+
         <Routes>
           <Route path="/notes/newNote" element={<AddNoteForm />} />
           <Route path="/notes" element={<NoteGrid />} />
